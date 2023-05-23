ee496fc36ed910711c1a50103d187a01f240be2e
Shekar Reddy <getbetterid@gmail.com>
2023-05-23 18:31:19 +0530

    Update MongodbScripte.js

--- MongodbScripte.js
+++ MongodbScripte.js
@@ -26,3 +26,44 @@ db.collectionName.aggregate([
         }
     }
 ])
+
+
+apply date filter
+
+db.collectionName.aggregate([
+    {
+        $match: {
+            $expr: {
+                $and: [
+                    { $eq: [ { $month: "$recorded date" }, YOUR_MONTH ] },  // replace YOUR_MONTH with the desired month (1-12)
+                    { $eq: [ { $year: "$recorded date" }, YOUR_YEAR ] }    // replace YOUR_YEAR with the desired year
+                ]
+            }
+        }
+    },
+    {
+        $group: {
+            _id: {
+                appname: "$appname",
+                platformName: "$platform name",
+                recordedDate: {
+                    $dateToString: { 
+                        format: "%Y-%m-%d", 
+                        date: "$recorded date" 
+                    }
+                }
+            },
+            totalMemoryUsed: {
+                $sum: "$memoryUsed"
+            }
+        }
+    },
+    {
+        $group: {
+            _id: "$_id.appname",
+            maxMemoryUsed: {
+                $max: "$totalMemoryUsed"
+            }
+        }
+    }
+])
