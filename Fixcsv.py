17f86fe855333baa2258b429ce6deb8a8a300484
Shekar Reddy <getbetterid@gmail.com>
2023-09-07 01:51:55 +0530

    Update Fixcsv.py

--- Fixcsv.py
+++ Fixcsv.py
@@ -10,8 +10,9 @@ input_file_path = sys.argv[1]
 output_file_path = input_file_path.rsplit('.', 1)[0] + '_corrected.csv'
 
 def fix_quotes(field):
-    # Replace each " inside the field with ""
-    return re.sub(r'"', '""', field)
+    # Escape quotes that are within the field but not surrounding it
+    field = re.sub(r'(?<!^)"(?!$)', '""', field)
+    return field
 
 with open(input_file_path, 'r', newline='', encoding='utf-8') as infile, \
         open(output_file_path, 'w', newline='', encoding='utf-8') as outfile:
