3aae4b9aebb9eb44300091aff8963c19bb2a28e5
Shekar Reddy <getbetterid@gmail.com>
2023-09-07 01:54:16 +0530

    Update Fixcsv.py

--- Fixcsv.py
+++ Fixcsv.py
@@ -2,6 +2,9 @@ import csv
 import sys
 import re
 
+def fix_quotes(field):
+    return re.sub(r'\"', '""', field)
+
 if len(sys.argv) < 2:
     print("Usage: python fixcsv.py <path_to_file.csv>")
     sys.exit(1)
@@ -9,18 +12,18 @@ if len(sys.argv) < 2:
 input_file_path = sys.argv[1]
 output_file_path = input_file_path.rsplit('.', 1)[0] + '_corrected.csv'
 
-def fix_quotes(field):
-    # Escape quotes that are within the field but not surrounding it
-    field = re.sub(r'(?<!^)"(?!$)', '""', field)
-    return field
-
 with open(input_file_path, 'r', newline='', encoding='utf-8') as infile, \
-        open(output_file_path, 'w', newline='', encoding='utf-8') as outfile:
+     open(output_file_path, 'w', newline='', encoding='utf-8') as outfile:
+
     reader = csv.reader(infile)
-    writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
-    
+    writer = csv.writer(outfile)
+
     for row in reader:
-        new_row = [fix_quotes(field) for field in row]
-        writer.writerow(new_row)
+        corrected_row = []
+        for field in row:
+            corrected_field = fix_quotes(field)
+            corrected_row.append(corrected_field)
+        
+        writer.writerow(corrected_row)
 
 print(f"Corrected CSV saved as {output_file_path}")
