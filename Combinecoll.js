a3a4895ec6160d0b670699bd3083244475bb02f3
Shekar Reddy <getbetterid@gmail.com>
2023-11-08 17:06:37 +0530

    Update Combinecoll.js

--- Combinecoll.js
+++ Combinecoll.js
@@ -26,3 +26,50 @@ db.cvr_record.aggregate([
         }
     }
 ]);
+
+
+//case insensitive match uaing regex
+
+db.cvr_record.aggregate([
+    {
+        $match: {
+            createdAt: {
+                $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1), // First day of the current month
+                $lte: new Date() // Current date
+            }
+        }
+    },
+    {
+        $lookup: {
+            from: "app_metadata",
+            let: { distributedAppId: "$distributedappId" }, // Define the local variable
+            pipeline: [
+                {
+                    $match: {
+                        $expr: {
+                            $regexMatch: {
+                                input: "$distributedappId", // Field from the app_metadata collection
+                                regex: "$$distributedAppId", // Local variable from the cvr_record collection
+                                options: "i" // Case-insensitive match
+                            }
+                        }
+                    }
+                }
+            ],
+            as: "app_metadata"
+        }
+    },
+    {
+        $unwind: {
+            path: "$app_metadata",
+            preserveNullAndEmptyArrays: true // This will keep records from cvr_record even if the lookup finds no matches in app_metadata
+        }
+    },
+    {
+        $merge: {
+            into: "combined_collection", // The target collection for merging
+            whenMatched: "merge",
+            whenNotMatched: "insert"
+        }
+    }
+]);
