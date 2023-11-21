41c2377d4e4bcca9359ebf9dd5d01767c1b8cf2d
Shekar Reddy <getbetterid@gmail.com>
2023-11-21 12:17:35 +0530

    Update Push.bat

--- Push.bat
+++ Push.bat
@@ -1,8 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Loop from 1 to 40
-for /L %%i in (1,1,40) do (
+REM Check if a loop count argument is provided
+if "%1"=="" (
+  echo Usage: push_apps.bat <loop_count>
+  exit /b 1
+)
+
+REM Get the loop count from the command-line argument
+set loop_count=%1
+
+REM Loop from 1 to the specified loop_count
+for /L %%i in (1,1,%loop_count%) do (
   set app_name=app%%i
   cf push !app_name! &
 )
