15c3587edbcef0bf669572938a88d2102ad3d9ff
Shekar Reddy <getbetterid@gmail.com>
2023-11-21 12:22:57 +0530

    Update Push.bat

--- Push.bat
+++ Push.bat
@@ -2,18 +2,18 @@
 setlocal enabledelayedexpansion
 
 REM Check if a loop count argument is provided
-if "%1"=="" (
+if "%~1"=="" (
   echo Usage: push_apps.bat <loop_count>
   exit /b 1
 )
 
 REM Get the loop count from the command-line argument
-set loop_count=%1
+set loop_count=%~1
 
 REM Loop from 1 to the specified loop_count
 for /L %%i in (1,1,%loop_count%) do (
   set app_name=app%%i
-  cf push !app_name! &
+  cf push !app_name! ^
 )
 
 REM Wait for all background jobs to finish
