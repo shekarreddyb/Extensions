27470269b176c2b79a9069165d392a1ec60e17b5
Shekar Reddy <getbetterid@gmail.com>
2025-04-29 00:11:04 +0530

    Create program.cs

---
+++ redistodo/program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows;
+
+namespace RedisTodoApp
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
